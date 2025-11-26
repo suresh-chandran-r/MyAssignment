09ae75785d0da3cd5a808059375f9784cf4f8804
suresh-chandran-r <sendsuresh@gmail.com>
2025-11-26 16:41:17 +0530

    Day1

---
+++ day1/dataTypes.js
@@ -0,0 +1,43 @@
+
+
+//Learn primitive data types in JavaScript
+
+/*Create the following variables using let (not using var) and check their typeOf 
+    a) firstName 
+    b) companyName 
+    c) mobileNumber 
+    d) isAutomation 
+    e) hasPlaywright (do not assign) 
+ Print and confirm the values and data types  */
+ 
+var x = "Hello";
+console.log(x);
+console.log(typeof x);
+
+
+let firstName = "Suresh";
+console.log(firstName);
+console.log(typeof firstName);
+
+let companyName = "TestLeaf"
+console.log(companyName);
+console.log(typeof companyName)
+
+let mobileNumber = "12456987"
+console.log(mobileNumber);
+console.log(typeof mobileNumber)
+
+//boolean datatype
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation)
+
+//undefined datatype
+let isUndefined //= null
+console.log(isUndefined);
+console.log(typeof isUndefined)
+
+//object datatype
+let isNull = null
+console.log(isNull);
+console.log(typeof isNull)
